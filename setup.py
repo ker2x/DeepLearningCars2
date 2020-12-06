c5e9d54698cd91a6afdc2de23dfca67bcf509700
Laurent Laborde <kerdezixe@gmail.com>
2020-12-06 16:53:06 +0100

    restructuring the project

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-# python setup.py build_ext --inplace --force
+setup(
+    name='DeepLearningCars2',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/ker2x/DeepLearningCars2',
+    license='WTFPL',
+    author='ker',
+    author_email='kerdezixe+github@gmail.com',
+    description='racing car with a mix of NN and genetic'
+)
 
 setup(
     ext_modules = cythonize(["simulation.pyx", "vector.pyx"],
